1570907d902a778bdd7cea5069e94ce5ded4f8af
ignaciofontaine <ignaciofontainebustos@gmail.com>
2023-10-20 11:44:23 -0300

    carpetas y Usuario creado

---
+++ api/src/models/User.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "user",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        unique: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 15],
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        isEmail: true,
+      }
+    },
+    { timestamps: false }
+  );
+};
