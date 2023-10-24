656097cbeda62f38f664b7fef83ca9029e8caef3
ignaciofontaine <ignaciofontainebustos@gmail.com>
2023-10-24 14:47:16 -0300

    configurando reducer

---
+++ client/src/Redux/reducer.js
@@ -0,0 +1,17 @@
+//Config initialState
+const initialState = {
+  
+};
+
+//Config reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+export default reducer;
