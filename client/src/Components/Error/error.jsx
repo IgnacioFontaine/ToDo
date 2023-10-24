f4db4e83bf77118391d161ec765ee29c9dbd4bfe
ignaciofontaine <ignaciofontainebustos@gmail.com>
2023-10-24 15:15:32 -0300

    error creado

---
+++ client/src/Components/Error/error.jsx
@@ -0,0 +1,17 @@
+import {Box,Typography } from "@mui/material"
+
+const Error = () => {
+
+  return (
+    <Box sx={{ height: "150vh", boxShadow: 2 }}>
+      <Box>
+
+        <Typography>Error 404</Typography>
+        <Typography>Return to home</Typography>
+        
+      </Box> 
+    </Box>
+  );
+};
+
+export default Error;
