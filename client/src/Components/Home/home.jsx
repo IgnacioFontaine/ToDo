ba9646aadd34ddcb9baf87fb7b6583c28bc9012e
ignaciofontaine <ignaciofontainebustos@gmail.com>
2023-10-24 15:02:10 -0300

    home creado

---
+++ client/src/Components/Home/home.jsx
@@ -0,0 +1,25 @@
+import {Box,Typography } from "@mui/material"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+
+const Home = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+
+  }, [dispatch]);
+
+  
+
+  return (
+    <Box sx={{ height: "150vh", boxShadow: 2 }}>
+      <Box>
+        <Typography>Soy el home</Typography>
+        
+      </Box> 
+    </Box>
+  );
+};
+
+export default Home;
