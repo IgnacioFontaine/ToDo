d189661781b65e5a98b078a24a9cdf7115e406f4
ignaciofontaine <ignaciofontainebustos@gmail.com>
2023-10-25 12:52:18 -0300

    primer action

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import ACTION_TYPES from './actionTypes'
 
 export const createTask = (task) => async (dispatch) => {
   const newtask = await axios.post("http://localhost:3001/task", task);
