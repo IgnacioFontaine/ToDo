1b7c3b26c6e82e14912d1fcf9a68d2f32cf34b51
ignaciofontaine <ignaciofontainebustos@gmail.com>
2023-10-26 12:50:28 -0300

    modify task succes

--- client/src/Redux/reducer.js
+++ client/src/Redux/reducer.js
@@ -3,10 +3,10 @@ import ACTION_TYPES from './actionTypes'
 //Config initialState
 const initialState = {
   users:[],
-  allTasks: [],
-  tasksByUser: [],
-  onTask_: [],
-  offTask_:[],
+  all_tasks: [],
+  tasks_by_user: [],
+  on_task: [],
+  off_task:[],
   
 };
 
@@ -16,8 +16,20 @@ const reducer = (state = initialState, action) => {
     case ACTION_TYPES.GET_ALL_TASKS:
       return {
         ...state,
-        allTasks: action.payload,
+        all_tasks: action.payload,
       };
+    
+    case ACTION_TYPES.MODIFY_TASK_SUCCESS:
+      state.all_tasks.forEach((task)=>{
+                if(task.id === action.payload.id){
+                    task.name = action.payload.name
+                    task.status = action.payload.status
+                }
+            })
+        return {
+            ...state,
+            all_tasks: [...state.all_tasks]
+        };
     
     default:
       return {
