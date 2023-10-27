2fc3cd9f6b6f7b9c9bb783ca01c313fc93781489
ignaciofontaine <ignaciofontainebustos@gmail.com>
2023-10-27 20:19:18 -0300

    error view linda

--- client/src/Components/Error/error.jsx
+++ client/src/Components/Error/error.jsx
@@ -6,18 +6,18 @@ const Error = () => {
   const navigate = useNavigate()
 
   return (
-    <Box sx={{ height: "150vh", boxShadow: 2 }}>
-      <Box>
+    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", bgcolor: "#A5A5A5", height: "100vh", width: "100vw", alignContent: "center" }}>
+      <Box sx={{ height: "35vh", width: "35vw", alignContent: "center", alignItems: "center", boxShadow: 2, m: 20, p: 5, bgcolor: "#E6E6E6", borderRadius: 5 }}>
+        <Box sx={{p:10}}>
         <Box>
           <Typography variant="h2">Error 404</Typography>
-
         </Box>
         <Box>
           <Typography variant="h4">Return to home</Typography>
           <Button onClick={()=>navigate("/")}>Home</Button>
-          
         </Box>
-      </Box> 
+      </Box>
+      </Box>
     </Box>
   );
 };
