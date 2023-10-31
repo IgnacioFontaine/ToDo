30beb5467b0e479f3b3ef86befab9ebed5810354
ignaciofontaine <ignaciofontainebustos@gmail.com>
2023-10-31 12:03:27 -0300

    primera config firebase

---
+++ client/src/Components/Firebase/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDe7k2j1wqhPn91nkVK5SLjlV_1hdJTRYw",
+  authDomain: "todo-754a5.firebaseapp.com",
+  projectId: "todo-754a5",
+  storageBucket: "todo-754a5.appspot.com",
+  messagingSenderId: "120662675857",
+  appId: "1:120662675857:web:564228a9dd235205778a30"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
