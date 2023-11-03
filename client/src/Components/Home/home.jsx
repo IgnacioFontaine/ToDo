510c77d4e62e42424c726115c5f4ca51bd4722cf
ignaciofontaine <ignaciofontainebustos@gmail.com>
2023-11-03 17:18:21 -0300

    redux listo

--- client/src/Components/Home/home.jsx
+++ client/src/Components/Home/home.jsx
@@ -2,24 +2,24 @@ import { Box, Typography } from "@mui/material"
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CreateTask from "../CreateTask/createTask";
-import { getTaskUser, setUser} from "../../Redux/actions"
+import { getTaskUser, setUser } from "../../Redux/actions"
 import Tasks from "../Tasks/tasks";
 import TasksOff from "../Tasks/tasksOff";
 
 const Home = () => {
   const dispatch = useDispatch()
   const user = useSelector((state) => state?.current_user);
+  const Tasks_on = useSelector((state) => state?.on_task);
+  const Tasks_off = useSelector((state) => state?.off_task);
 
   useEffect(() => {
-    dispatch(getTaskUser(user));
     dispatch(setUser(user));
-
+    dispatch(getTaskUser(user));
     
-  }, [dispatch, user]);
+  }, [dispatch,user,Tasks_off, Tasks_on]);
 
   
-  const Tasks_on = useSelector((state) => state?.on_task)
-  const Tasks_off = useSelector((state) => state?.off_task)
+  
 
 
 
