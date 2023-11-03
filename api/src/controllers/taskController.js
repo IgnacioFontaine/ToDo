81c88b14cbd65a93198fb4dd2fd331cf2920f25e
ignaciofontaine <ignaciofontainebustos@gmail.com>
2023-11-03 16:21:24 -0300

    cambio rutas

--- api/src/controllers/taskController.js
+++ api/src/controllers/taskController.js
@@ -39,6 +39,34 @@ const getTasksByUser = async (user) => {
   }
 };
 
+const getTasksByUserOn = async (user) => {
+  try {
+    let tasks = await Task.findAll({
+      where: {
+        user: user,
+        status:"ON"
+      },
+    });
+    return tasks;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const getTasksByUserOff = async (user) => {
+  try {
+    let tasks = await Task.findAll({
+      where: {
+        user: user,
+        status:"OFF"
+      },
+    });
+    return tasks;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 const getTasksByStatus = async (status) => {
   try {
     let tasks = await Task.findAll({
@@ -110,5 +138,7 @@ module.exports = {
   updateStatusTask,
   updateTask,
   getTasksByStatus,
-  getTasksByUser
+  getTasksByUser,
+  getTasksByUserOn,
+  getTasksByUserOff
 };
